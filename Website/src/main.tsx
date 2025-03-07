b28035d212d9928f8915b4819332f63092de3372
sam <Squishy47@users.noreply.github.com>
2025-03-07 08:43:56 +0000

    deleted a bunch of comps and packages

--- Website/src/main.tsx
+++ Website/src/main.tsx
@@ -7,15 +7,12 @@ import "./globals.css";
 import { Theme, ThemeProvider } from "./components/ui/ThemeProvider.tsx";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/Router.tsx";
-import { SidebarProvider } from "./components/ui/sidebar.tsx";
 
 function Main() {
   return (
     <StrictMode>
       <ThemeProvider defaultTheme={Theme.System} storageKey="vite-ui-theme">
-        <SidebarProvider>
-          <RouterProvider router={router} />
-        </SidebarProvider>
+        <RouterProvider router={router} />
       </ThemeProvider>
     </StrictMode>
   );
