1166ddb2ff15db6e4bc3fd9c0a84de6fcbdc1a73
sam <Squishy47@users.noreply.github.com>
2025-03-07 09:16:38 +0000

    setup pre commit hooks

--- Website/src/components/shadcn/ThemeProvider.tsx
+++ Website/src/components/shadcn/ThemeProvider.tsx
@@ -4,99 +4,114 @@ import { Button } from "./button";
 import { Moon, Sun, SunMoon } from "lucide-react";
 
 export enum Theme {
-    Dark = "dark",
-    Light = "light",
-    System = "system",
+  Dark = "dark",
+  Light = "light",
+  System = "system",
 }
 
 type ThemeProviderProps = {
-    children: React.ReactNode;
-    defaultTheme?: Theme;
-    storageKey?: string;
+  children: React.ReactNode;
+  defaultTheme?: Theme;
+  storageKey?: string;
 };
 
 type ThemeProviderState = {
-    theme: Theme;
-    setTheme: (theme: Theme) => void;
+  theme: Theme;
+  setTheme: (theme: Theme) => void;
 };
 
 const initialState: ThemeProviderState = {
-    theme: Theme.System,
-    setTheme: () => null,
+  theme: Theme.System,
+  setTheme: () => null,
 };
 
 const ThemeProviderContext = createContext<ThemeProviderState>(initialState);
 
-export function ThemeProvider({ children, defaultTheme = Theme.System, storageKey = "vite-ui-theme", ...props }: ThemeProviderProps) {
-    const [theme, setTheme] = useState<Theme>(() => (localStorage.getItem(storageKey) as Theme) || defaultTheme);
+export function ThemeProvider({
+  children,
+  defaultTheme = Theme.System,
+  storageKey = "vite-ui-theme",
+  ...props
+}: ThemeProviderProps) {
+  const [theme, setTheme] = useState<Theme>(
+    () => (localStorage.getItem(storageKey) as Theme) || defaultTheme,
+  );
 
-    useEffect(() => {
-        const root = window.document.documentElement;
+  useEffect(() => {
+    const root = window.document.documentElement;
 
-        root.classList.remove("light", "dark");
+    root.classList.remove("light", "dark");
 
-        if (theme === "system") {
-            const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+    if (theme === "system") {
+      const systemTheme = window.matchMedia("(prefers-color-scheme: dark)")
+        .matches
+        ? "dark"
+        : "light";
 
-            root.classList.add(systemTheme);
-            return;
-        }
+      root.classList.add(systemTheme);
+      return;
+    }
 
-        root.classList.add(theme);
-    }, [theme]);
+    root.classList.add(theme);
+  }, [theme]);
 
-    const value = {
-        theme,
-        setTheme: (theme: Theme) => {
-            localStorage.setItem(storageKey, theme);
-            setTheme(theme);
-        },
-    };
+  const value = {
+    theme,
+    setTheme: (theme: Theme) => {
+      localStorage.setItem(storageKey, theme);
+      setTheme(theme);
+    },
+  };
 
-    return (
-        <ThemeProviderContext.Provider {...props} value={value}>
-            {children}
-        </ThemeProviderContext.Provider>
-    );
+  return (
+    <ThemeProviderContext.Provider {...props} value={value}>
+      {children}
+    </ThemeProviderContext.Provider>
+  );
 }
 
 export const useTheme = () => {
-    const context = useContext(ThemeProviderContext);
+  const context = useContext(ThemeProviderContext);
 
-    if (context === undefined) throw new Error("useTheme must be used within a ThemeProvider");
+  if (context === undefined)
+    throw new Error("useTheme must be used within a ThemeProvider");
 
-    return context;
+  return context;
 };
 
 export function ThemeToggle({ className = "" }: { className?: string }) {
-    const { setTheme, theme } = useTheme();
+  const { setTheme, theme } = useTheme();
 
-    const toggleTheme = () => {
-        if (theme === Theme.Light) {
-            setTheme(Theme.Dark);
-            return;
-        } else if (theme === Theme.Dark) {
-            setTheme(Theme.System);
-            return;
-        } else if (theme === Theme.System) {
-            setTheme(Theme.Light);
-            return;
-        }
-    };
+  const toggleTheme = () => {
+    if (theme === Theme.Light) {
+      setTheme(Theme.Dark);
+      return;
+    } else if (theme === Theme.Dark) {
+      setTheme(Theme.System);
+      return;
+    } else if (theme === Theme.System) {
+      setTheme(Theme.Light);
+      return;
+    }
+  };
 
-    const showIcon = () => {
-        if (theme === Theme.Light) {
-            return <Sun />;
-        } else if (theme === Theme.Dark) {
-            return <Moon />;
-        } else if (theme === Theme.System) {
-            return <SunMoon />;
-        }
-    };
+  const showIcon = () => {
+    if (theme === Theme.Light) {
+      return <Sun />;
+    } else if (theme === Theme.Dark) {
+      return <Moon />;
+    } else if (theme === Theme.System) {
+      return <SunMoon />;
+    }
+  };
 
-    return (
-        <Button variant="outline" className={className} onClick={() => toggleTheme()}>
-            {showIcon()}
-        </Button>
-    );
+  return (
+    <Button
+      variant="outline"
+      className={className}
+      onClick={() => toggleTheme()}
+    >
+      {showIcon()}
+    </Button>
+  );
 }
--- Website/src/types.ts
+++ Website/src/types.ts
@@ -1,25 +1,25 @@
 export type Task = {
-    id: string;
-    name: string;
-    start_time: string;
-    end_time: string;
-    person_id: string;
+  id: string;
+  name: string;
+  start_time: string;
+  end_time: string;
+  person_id: string;
 };
 
 export type Person = {
-    id: string;
-    name: string;
+  id: string;
+  name: string;
 };
 
 export type Event = {
-    id: string;
-    name: string;
-    date: string;
+  id: string;
+  name: string;
+  date: string;
 };
 
 export type CreateEvent = {
-    name: string;
-    start_time: string;
-    end_time: string;
-    user_id: string;
+  name: string;
+  start_time: string;
+  end_time: string;
+  user_id: string;
 };
