6223406da0128fc01bc6f43c1ac120edc6c8d64d
sam <Squishy47@users.noreply.github.com>
2025-03-07 09:25:11 +0000

    prettier

--- projects/Backend/src/context.ts
+++ projects/Backend/src/context.ts
@@ -5,7 +5,7 @@ export async function GlobalContext(data: any) {
 
   const supabase = createClient(
     process.env.SUPABASE_URL!,
-    process.env.SUPABASE_SERVICE_ROLE_KEY!
+    process.env.SUPABASE_SERVICE_ROLE_KEY!,
   );
 
   let user: User | null = null;
